dadb5dec53515d54f5f3e5c77a4404ca42cc120f
AlmudenaRA <almudena.ruedas@gmail.com>
2021-06-13 18:44:59 +0200

    Añadidos sonidos

--- Assets/Scripts/CollectAnimation.cs
+++ Assets/Scripts/CollectAnimation.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class CollectAnimation : MonoBehaviour
 {
+
+    public AudioSource audio;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -14,6 +18,7 @@ public class CollectAnimation : MonoBehaviour
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
             //Eliminar todo en 5 segundos
             Destroy(gameObject, 0.5f);
+            audio.Play();
         }
     }
 }
